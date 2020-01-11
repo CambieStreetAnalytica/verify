36f3d8931d7e1e9b0a8a0c7fbe0d67456af47338
Declan Herbertson <declanherbertson@gmail.com>
2020-01-11 14:35:20 -0800

    example chrome extension

---
+++ chrome-extension/background.js
@@ -0,0 +1,16 @@
+// run when first installed
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          // extension only active on this page
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
