596a2c9ab3f139004f10e11244b26059f10d13cf
Declan Herbertson <declanherbertson@gmail.com>
2020-01-12 00:50:41 -0800

    extension mvp, looks decent, comminicates with server

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,16 +1,21 @@
-let changeColor = document.getElementById('changeColor');
+chrome.tabs.getSelected(null,function(tab) {
+  const Http = new XMLHttpRequest();
+  console.log(tab.url);
+  const url='https://declanh.api.stdlib.com/verify2@dev/' + '?url=' + tab.url;
+  Http.open("GET", url);
+  Http.send();
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+  Http.onreadystatechange = (e) => {
+    let res = JSON.parse(Http.response);
+    let insertSummary = document.getElementById("insertSummary")
+    let insertBias = document.getElementById("insertBias");
+    let spinner = document.getElementById("spinner");
+    let analyzing = document.getElementById("analyzing");
+    insertSummary.classList.add('insertedSummary');
+    insertSummary.innerHTML = res['text'];
+    insertBias.innerHTML = res['bias'];
+    analyzing.innerHTML = "Analyzed Page";
+    spinner.classList.add("none");
+  }
 });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});  
-          
-    });
-  };
