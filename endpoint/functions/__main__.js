e5a0753a8c3435543df333a46e05c8a92a77c37f
Declan Herbertson <declanherbertson@gmail.com>
2020-01-11 20:54:34 -0800

    endpoint 8:54pm

---
+++ endpoint/functions/__main__.js
@@ -0,0 +1,129 @@
+/**
+* verify endpoint
+*/
+const deepai = require('deepai');
+module.exports = async (url = '', data = '') => {
+  //http://declanh.api.stdlib.com/verify@dev/?url=hello&data=hi
+  // test url: https://www.foxnews.com/politics/fbi-director-wray-deeply-regrets-fisa-court-errors-in-trump-russia-probe
+  if (url === '') {
+    return {"status": 400, "text": 'please provide a url'};
+  }
+  let articleArray = await getArticleText(url);
+  let articleText = articleArray[0];
+  let siteName = articleArray[1];
+  let bias = await getBias(siteName);
+  let summary = await summarizeArticle(articleText);
+  return {'status': 200, 'text': summary.length > 5 ? summary : articleText, 'bias': bias};
+};
+
+const getArticleText = async (url = '', retry=false) => {
+  if (url === '') {
+    return 'NULL';
+  }
+  var outlineOptions = {
+    host: 'api.diffbot.com',
+    path: `/v3/article?token=${process.env.OUTLINE_KEY}&url=${url}`,
+    method: 'GET'
+  };
+  let res = await makeHttpCall(outlineOptions);
+  try {
+    let siteName = res['objects'][0]['siteName'];
+    text = res['objects'][0]['text'];
+    text = text.replace(/(\r\n|\n|\r)/gm, "");
+    text = text.replace(/(\\|\")/gi, '');
+    return [text, siteName];
+  } catch (e) {
+    if (retry === true) {
+      return ['NULL', url];
+    }
+    return await getArticleText(url, true);
+  }
+};
+
+const getBias = async (url, retry=false) => {
+  // https://mediabiasfactcheck.com/search/foxnews/feed/rss2
+  url = url.split(" ").join("+");
+  site = getSite(url);
+  biasTopLevelOptions = {
+    host: 'mediabiasfactcheck.com',
+    path: `/search/${site}/feed/rss2`,
+    method: 'GET'
+  };
+  let biasSearch = await fetchWebPage(biasTopLevelOptions);
+  try {
+    let bias = biasSearch.split("[CDATA[Media Bias Fact Check]]")[1].split("<description><![CDATA[<p>")[1];
+    bias = bias.toLowerCase();
+    bias = bias.split('bias')[0];
+    bias = bias[0].toUpperCase() + bias.substring(1) + "Bias";
+    return bias;
+    if (bias.length() > 25) {
+      return "Unknown";
+    }
+  } catch (e) {
+    if (retry === true) {
+      return "Unknown";
+    }
+    return await getBias(url, true);
+  }
+}
+
+const getSite = (url) => {
+  url = url.split("https://").length > 1 ? url.split("https://")[1] : url;
+  url = url.split("http://").length > 1 ? url.split("http://")[1] : url;
+  url = url.split(".").length > 2 ? url.split(".")[1] : url.split(".")[0];
+  return url;
+}
+
+const summarizeArticle = async (text, retry=false) => {
+  if (text === null || text === undefined || text === '' || typeof text === 'object' || text.length < 200) {
+    return "NULL";
+  }
+  deepai.setApiKey(`${process.env.SUMMARIZATION_KEY}`);
+  let resp = await deepai.callStandardApi("summarization", {
+    text: text,
+  });
+  try {
+    return resp['output'];
+  } catch (e) {
+    if (retry === true) {
+      return "NULL";
+    }
+    return await summarizeArticle(text, true);
+  }
+}
+
+const makeHttpCall = async (options, format) => {
+	return new Promise((resolve) => {
+		var req = https.request(options, res => {
+			res.setEncoding('utf8');
+			var returnData = "";
+			res.on('data', chunk => {
+			returnData = returnData + chunk;
+			});
+			res.on('end', () => {
+			let results = JSON.parse(returnData);
+			resolve(results);
+			});
+		});
+		if (options.method == 'POST' || options.method == 'PATCH') {
+			req.write(JSON.stringify(options.body));
+		}
+		req.end();
+	})
+};
+
+const fetchWebPage = async (options, format) => {
+	return new Promise((resolve) => {
+		var req = https.request(options, res => {
+			res.setEncoding('utf8');
+			var returnData = "";
+      res.once('readable', () => {
+       res.on('data', (chunk) => { 
+         returnData += chunk;
+         resolve(returnData);
+      });  
+    })
+	});
+	req.end();
+	})
+};
