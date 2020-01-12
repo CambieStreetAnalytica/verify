1900865e4dc4cb813992e5664497447ab27a8cfe
Declan Herbertson <declanherbertson@gmail.com>
2020-01-12 10:55:25 -0800

    adding sentiment to endpoint

--- endpoint/functions/__main__.js
+++ endpoint/functions/__main__.js
@@ -2,6 +2,7 @@
 * verify endpoint
 */
 const deepai = require('deepai');
+const language = require('@google-cloud/language');
 module.exports = async (url = '', data = '') => {
   //http://declanh.api.stdlib.com/verify@dev/?url=hello&data=hi
   // test url: https://www.foxnews.com/politics/fbi-director-wray-deeply-regrets-fisa-court-errors-in-trump-russia-probe
@@ -11,9 +12,22 @@ module.exports = async (url = '', data = '') => {
   let articleArray = await getArticleText(url);
   let articleText = articleArray[0];
   let siteName = articleArray[1];
+  let sentiment = await getSentiment(articleText);
+  //return await biasTest(siteName);
   let bias = await getBias(siteName);
   let summary = await summarizeArticle(articleText);
-  return {'status': 200, 'text': summary, 'bias': bias};
+  return {'status': 200, 'text': summary, 'bias': bias, 'sentiment': sentiment};
+};
+
+const getSentiment = async (text) => {
+  const client = new language.LanguageServiceClient();
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };  
+  const [result] = await client.analyzeSentiment({document: document});
+  const sentiment = result.documentSentiment;
+  return sentiment
 };
 
 const getArticleText = async (url = '', retry=false) => {
@@ -52,10 +66,15 @@ const getBias = async (url, retry=false) => {
   let biasSearch = await fetchWebPage(biasTopLevelOptions);
   try {
     let bias = biasSearch.split("[CDATA[Media Bias Fact Check]]")[1].split("<description><![CDATA[<p>")[1];
+    let len = 0;
+    while (bias[len] === " " || bias[len] === "-" || bias[len].toUpperCase() === bias[len]) {
+      len++;
+    }
+    bias = bias.substring(0, len > 1 ? len - 2 : len);
     bias = bias.toLowerCase();
-    bias = bias.split('bias')[0];
+    // bias = bias.split('bias')[0];
     bias = bias[0].toUpperCase() + bias.substring(1);
-    return bias;
+    return bias
   } catch (e) {
     if (retry === true) {
       return "Unknown";
@@ -124,3 +143,35 @@ const fetchWebPage = async (options, format) => {
 	req.end();
 	})
 };
+
+
+// tests 
+const articleTest = async (url) => {
+  if (url === '') {
+    return 'NULL';
+  }
+  var outlineOptions = {
+    host: 'api.diffbot.com',
+    path: `/v3/article?token=${process.env.OUTLINE_KEY}&url=${url}`,
+    method: 'GET'
+  };
+  let res = await makeHttpCall(outlineOptions);
+  return res;
+}
+
+const biasTest = async (url) => {
+   url = url.split(" ").join("+");
+  site = getSite(url);
+  biasTopLevelOptions = {
+    host: 'mediabiasfactcheck.com',
+    path: `/search/${site}/feed/rss2`,
+    method: 'GET'
+  };
+  let biasSearch = await fetchWebPage(biasTopLevelOptions);
+  let bias = biasSearch.split("[CDATA[Media Bias Fact Check]]")[1].split("<description><![CDATA[<p>")[1];
+  let len = 0;
+  while (bias[len] === " " || bias[len] === "-" || bias[len].toUpperCase() === bias[len]) {
+    len++;
+  }
+  return bias.substring(0, len > 1 ? len - 2 : len);
+}
