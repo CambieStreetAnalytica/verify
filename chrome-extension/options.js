9ecad990116ab5110058c4d78e58d690fa607317
Declan Herbertson <declanherbertson@gmail.com>
2020-01-12 01:39:24 -0800

    added local caching and option to clear cache

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,15 +1,7 @@
-let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
-    }
-  }
-  constructOptions(kButtonColors);
+
+      document.getElementById("clearStorage").onclick = function() {
+        chrome.storage.sync.clear(()=>{
+          console.log("Cache Cleared")
+          document.getElementById("clearText").innerHTML = "Storage Cleared";
+        });
+      };
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,20 +2,38 @@ chrome.tabs.getSelected(null,function(tab) {
   const Http = new XMLHttpRequest();
   console.log(tab.url);
   const url='https://declanh.api.stdlib.com/verify2@dev/' + '?url=' + tab.url;
-  Http.open("GET", url);
-  Http.send();
+  chrome.storage.sync.get(url, (value) => {
+    if (url in value) {
+      propagateData(value[url]['text'], value[url]['bias'], url);
+      console.log("fetched locally");
+    } else {
+      Http.open("GET", url);
+      Http.send();
+    }
+  });
 
   Http.onreadystatechange = (e) => {
     let res = JSON.parse(Http.response);
-    let insertSummary = document.getElementById("insertSummary")
-    let insertBias = document.getElementById("insertBias");
-    let spinner = document.getElementById("spinner");
-    let analyzing = document.getElementById("analyzing");
-    insertSummary.classList.add('insertedSummary');
-    insertSummary.innerHTML = res['text'];
-    insertBias.innerHTML = res['bias'];
-    analyzing.innerHTML = "Analyzed Page";
-    spinner.classList.add("none");
+    propagateData(res['text'], res['bias'], url, status=200);
+    console.log("fetched remotely");
   }
 });
+
+function propagateData(text, bias, url, status=200) {
+  chrome.storage.sync.set({[url]: {'bias': bias, 'text': text}}, ()=>{})
+  let insertSummary = document.getElementById("insertSummary")
+  let insertBias = document.getElementById("insertBias");
+  let spinner = document.getElementById("spinner");
+  let analyzing = document.getElementById("analyzing");
+  insertSummary.classList.add('insertedSummary');
+  if (status == 200) {
+    insertSummary.innerHTML = text != "NULL" ? text : "Summary Unavailable";
+    insertBias.innerHTML = bias != "NULL" ? bias : "Unavailable";
+  } else {
+    insertSummary.innerHTML = "Unavailable";
+    insertBias.innerHTML = "Summary Unavailable"
+  }
+  analyzing.innerHTML = "Analyzed Page";
+  spinner.classList.add("none");
+}
 
